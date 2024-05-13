f0d24a53c428ee2a517d7a9afc2d8a2c2cc3aa4b
SAADTARIQ2503 <111899602+SAADTARIQ2503@users.noreply.github.com>
2024-05-14 00:55:17 +0500

    Update User.h

--- Headers/User.h
+++ Headers/User.h
@@ -1,46 +1,42 @@
 #pragma once
 #include <string>
 #include <iostream>
-#include "Headers/Repository.h"
 
+using namespace std;
 
+class Admin;
 class graph;
-template<typename T>
+class RepoAvl;
 class HashTable;
 
 class User
 {
-private:
+public:
 
 	string username;
 	string password;
-	Repo* repository;
-	int numFollowers;
-	int numFollowing;
 
 public:
-	User(string Username, string Pass);
-	User(string Username, string Pass, int follower, int numfollowing);
+	User(const string Username, const string Pass);
+	User(const string Username, const string Pass, int follower, int numfollowing);
 	User(const User& User2);
 	User();
-	~User();
-	void printrepo(const string& username, HashTable<User>& table);
-	void printAllrepo(const string& username, HashTable<User>& table);
-	void registerUser(const string& username, const string& password, HashTable<User>& table, graph& Users);
-	void AddRepository(const string& username, const string& reponame, int visibility, HashTable<User>& table);
-	void AddCommit(const string& username, const string& reponame,string commit, HashTable<User>& table);
-	void AddFile(const string& username, const string& reponame, string commit, HashTable<User>& table);
-	void removeRepository(const string& username, string& reponame, HashTable<User>& table);
-	bool loginUser(const string& username, const string& password, HashTable<User>& table);
-	void followUser(User& followedUser, graph& Users);
-	void unfollowUser(User& followedUser, graph& Users);
-	User* getUserProfile(const string& username, HashTable<User>& table);
-	void display() const;
-	string GetUsername() const;
-	string GetPasssword() const;
-	Repo* Getrepo();
-	int GetFollowers() const;
-	int GetFollowing() const;
-	
+	string GetUsername();
+	string GetPasssword();
+	void printrepo(const string& username, Admin& AdminUser);
+	void printAllrepo(const string& username, Admin& AdminUser);
+	void registerUser(string& username, string& password, Admin& AdminUser);
+	void AddRepository(const string& username, const string& reponame, int visibility, Admin& AdminUser);
+	void AddCommit(const string& username, const string& reponame, string commit, Admin& AdminUser);
+	void ShowCommit(const string& username, const string& reponame, Admin& AdminUser);
+
+	void AddFile(const string& username, const string& reponame, string file, Admin& AdminUser);
+	void ShowFile(const string& username, const string& reponame,  Admin& AdminUser);
+	void removeRepository(const string& username, string& reponame, Admin& AdminUser);
+	bool loginUser(const string& username, const string& password, Admin& AdminUser);
+	void followUser(string& follower, string& followed, Admin& AdminUser);
+	void unfollowUser(string& follower, string& followed, Admin& AdminUser);
+	User* getUserProfile(const string& username, Admin& AdminUser);
+
 
 };
