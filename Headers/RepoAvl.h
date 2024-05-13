1498b07458b38db5e87cc8521727ee049c8d7bdb
SAADTARIQ2503 <111899602+SAADTARIQ2503@users.noreply.github.com>
2024-05-14 00:52:50 +0500

    Update RepoAvl.h

--- Headers/RepoAvl.h
+++ Headers/RepoAvl.h
@@ -4,6 +4,7 @@
 
 class RepoNode {
 public:
+	string OwnerName;
 	string repoName;
 	int visibility_status;
 	MyList<string>* commits;
@@ -11,18 +12,20 @@ public:
 	RepoNode* left;
 	RepoNode* right;
 	int height;
-	RepoNode(string value, int vis);
+	RepoNode(string name, string rep, int vis);
 	RepoNode();
+	MyList<string>* getCommits();
+	MyList<string>* getFiles();
 };
 
 
 class RepoAvl
 {
 private:
-	RepoNode* root;
 	
+	RepoNode* root;
 	int balnceFactor(RepoNode*& ptr);
-	void insertVal(RepoNode*& ptr, string value, int visibile);
+	void insertVal(RepoNode*& ptr, string name, string rep, int visibile);
 	void SingleRotateLeft(RepoNode*& K1);
 	void SingleRotateRight(RepoNode*& K2);
 	void DoubleRotateRight(RepoNode*& K3);
@@ -32,8 +35,6 @@ private:
 	RepoNode* searchNode(string val, RepoNode*& ptr);
 	void inorder(RepoNode* ptr);
 	void inorderPublic(RepoNode* ptr);
-	void postorder(RepoNode* ptr);
-	void preorder(RepoNode* ptr);
 	string findMin(RepoNode* ptr);
 	int len(RepoNode* ptr);
 	void deleteNode(RepoNode*& ptr);
@@ -41,14 +42,22 @@ private:
 public:
 	// Constructor
 	RepoAvl();
-	void insert(string value, int visibile);
+	void insert(string name,string rep, int visibile);
 	RepoNode* search(string repoName);
 	string findMin();
 	int length();
 	void InorderPrint();
 	void InorderPrintPublic();
-	void preorderPrint();
-	void postorderPrint();
 	void remove(string repoName);
+	void AddFile(string repoName, string filename);
+	void Addcommit(string repoName, string commit);
+	RepoNode* getRoot();
+	MyList<string>* getCommits();
+	MyList<string>* getFiles();
+
+	
 };
 
+
+RepoAvl* searchUsername(MyList<RepoAvl>& L, string Username);
+RepoAvl* searchRep(MyList<RepoAvl> &L, string repName);
